c0c466865e0e5412c143cbec697470f10f19bafa
Kevin <913186441@qq.com>
2024-03-07 22:42:49 +0800

    Add comment example.

--- code/main.cpp
+++ code/main.cpp
@@ -18,6 +18,13 @@ struct Robot
     }
 }robot[robot_num + 10];
 
+/* 
+    @brief:泊位
+    @params: x,y: 泊位坐标; transport_time: 泊位运输到虚拟点的时间（单位：帧，1~1000）;
+             loading_speed: 每帧可装载物品数（1~5）
+    @ret: none
+*/
+
 struct Berth
 {
     int x;
