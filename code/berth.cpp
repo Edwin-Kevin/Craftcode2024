b31f76c159c9dc3bbf8637525e14b47dcbc4a671
Kevin <913186441@qq.com>
2024-03-14 21:12:16 +0800

    Fixed a bug: When the boat 0 parking in the berth, the berth won't be selected.

--- code/berth.cpp
+++ code/berth.cpp
@@ -14,7 +14,7 @@ int nearest_berth(int x, int y)
     int min = 400;
     for(int i = 0; i < berth_num; i++)
     {
-        if(((abs(berth[i].x - x) + abs(berth[i].y - y)) < min) && berth[i].boat_index > 0)
+        if(((abs(berth[i].x - x) + abs(berth[i].y - y)) < min) && berth[i].boat_index >= 0)
         {
             min = abs(berth[i].x - x) + abs(berth[i].y - y);
             nearestid = i;
